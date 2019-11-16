98a34a27c135687fac08f5ef7fcdda0101419881
DinaraKelyamova30 <kelyamova30@gmail.com>
2019-11-16 14:27:13 +0200

    jsCore

--- index.js
+++ index.js
@@ -1,55 +1,29 @@
-//button 'edit' mouseover/mouseout/click
-let edit = document.getElementsByClassName('edit');
-edit[0].addEventListener('mouseover', function () {
-    edit[0].style.backgroundColor = 'rgb(201, 204, 0)';
-    edit[0].style.border = '2px solid orange';
-})
-edit[0].addEventListener('mouseout', function () {
-    edit[0].style.backgroundColor = 'orange';
-})
+const outputArea = document.querySelector('.output');
+const inputEdit = document.querySelector('.textarea');
+const editBtn = document.querySelector('.editBtn');
+const editBlock = document.querySelector('.editBlock');
+const styleBlock = document.querySelector('.styleBlock');
+const radio = document.querySelector('.radio')
 
-edit[0].addEventListener('click', function (click) {
-    edit[0].style.backgroundColor = 'rgb(255, 102, 0)';
-    edit[0].style.color = 'white';
-    edit[0].style.border = 'rgb(255, 136, 0)';
 
-    //create textarea inside div class = input
-    let textArea = document.createElement('div');
-    let text = document.createElement('textarea');
-    text.style.width = '800px';
-    text.style.height = '200px';
-    text.style.marginTop = '20px';
-    text.style.marginLeft = '50px';
-    textArea.appendChild(text);
-    document.getElementById('textEditor').appendChild(textArea);
+function activeEdit() {
+    editBlock.style.display = 'block';
+    styleBlock.style.display = 'none';
+    inputEdit.value = outputArea.innerHTML;
+}
 
-    // creat button 'add' indide div class = input
-    let butAdd = document.createElement('button');
-    butAdd.style.width = '60px';
-    butAdd.style.height = '30px';
-    butAdd.style.backgroundColor = 'red';
-    butAdd.style.border = 'none';
-    butAdd.style.borderRadius = '5px';
-    butAdd.style.marginLeft = '50px';
-    butAdd.style.color = 'white';
-    butAdd.textContent = 'add';
-    document.getElementById('textEditor').appendChild(butAdd);
+function save() {
+    outputArea.innerHTML = inputEdit.value;
+    editBlock.style.display = 'none';
+}
 
-    // creat button 'save' indide div class = input
-    let butSave = document.createElement('button');
-    butSave.style.width = '60px';
-    butSave.style.height = '30px';
-    butSave.style.backgroundColor = 'green';
-    butSave.style.border = 'none';
-    butSave.style.borderRadius = '5px';
-    butSave.style.marginLeft = '5px';
-    butSave.style.color = 'white';
-    butSave.textContent = 'save';
-    document.getElementById('textEditor').appendChild(butSave);
-})
+function activeStyle() {
+    styleBlock.style.display = 'block';
+    editBlock.style.display = 'none';
+}
 
-edit[0].addEventListener('mouseout', function () {
-    edit[0].style.backgroundColor = 'rgb(255, 102, 0)';
-})
-
-//button 'style' mouseover/mouseout/click
+for (let i = 0; i < radio.select.length; i++) {
+    radio.select[i].onclick = function () {
+        outputArea.style.fontSize = event.target.value;
+    }
+}
